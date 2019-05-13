964bf8ec369e685ae1fba8391762ebcec626d959
Arianna Choza <ariannachoza@Ariannas-MacBook-Pro.local>
2019-05-13 17:27:54 -0400

    added sign up form, login, create account. has errors

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,13 +1,100 @@
 import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
+import { Routes } from '../constants/Routes';
 
-class SignUp extends Component{
+const SignUpPage = () => {
+   return (
+    <div>
+        <h1>Sign Up</h1>
+        <SignUpForm/>
+    </div>
+
+   )
+}
+export interface Props {
+    username?: string,
+    email?: string,
+    passwordOne?: string,
+    passwordTwo?: string,
+    error?: {}
+}
+export interface State {
+    username: string,
+    email: string,
+    passwordOne: string,
+    passwordTwo: string,
+    error: {}
+}
+const INITIAL_STATE = {
+    username : '',
+    email: '',
+    passwordOne: '',
+    passwordTwo: '',
+    error: null,
+  };
+
+class SignUpForm extends Component<State, Props>{
+    constructor(props:any){
+        super(props)
+        this.state = {...INITIAL_STATE}
+    }
+    onSubmit = (event:any) => {
+
+    }
+
+    onChange = (event:any) => {
+        this.setState({ [event.target.name]: event.target.value })
+    }
+
     render(){
+        const  {username, email, passwordOne, passwordTwo, error } = this.state
         return(
-            <div>
-                <h1>SignUp</h1>
-            </div>
+            <form onSubmit={this.onSubmit}>
+                <input
+                name="username"
+                value={username}
+                onChange={this.onChange}
+                type="text"
+                placeholder="Full Name"
+                />
+                <input
+                name="email"
+                value={email}
+                onChange={this.onChange}
+                type="text"
+                placeholder="Email Address"
+                />
+                <input
+                name="passwordOne"
+                value={passwordOne}
+                onChange={this.onChange}
+                type="password"
+                placeholder="Password"
+                />
+                <input
+                name="passwordTwo"
+                value={passwordTwo}
+                onChange={this.onChange}
+                type="password"
+                placeholder="Confirm Password"
+                />
+                <button type="submit">Sign Up</button>
+
+                {error && <p>{error.message}</p>}
+
+            </form>
         )
     }
 }
+ 
+const SignUpLink = () => {
+    return(
+        <p>
+        Don't have an account? <Link to={Routes.SIGN_UP}>Sign Up</Link>
+    </p>
+    )
+    
+}
 
-export default SignUp 
+export default SignUpPage
+export {SignUpForm, SignUpLink} 
